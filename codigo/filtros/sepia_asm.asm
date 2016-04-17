53047324254e8ca1ff818ec06fe726e5dfcca675
nicoluce <nicolas.p.luce@gmail.com>
2016-04-17 20:25:05 -0300

    Groudon

--- codigo/filtros/sepia_asm.asm
+++ codigo/filtros/sepia_asm.asm
@@ -1,4 +1,7 @@
-section .data
+section .rodata
+	val0503: dq 0.5, 0.3
+	val02: dq 0.2
+
 DEFAULT REL
 
 section .text
@@ -13,7 +16,6 @@ global sepia_asm
  %define FILAS rcx
 
 sepia_asm:
-;COMPLETAR
 	push rbp
 	mov rbp, rsp
 	push r12
@@ -23,7 +25,7 @@ sepia_asm:
 	push rbx
 	sub rsp, 8
 
-	xor i, i	; r12 = i + Offset_y = Offset_y
+	xor i, i	; r12 = i = 0
 
 
 	mov r14, COLS
@@ -31,7 +33,7 @@ sepia_asm:
 	mov r15, DST
 
 	.ciclo_filas:
-		xor j, j	; r13 = j + Offset_x = Offset_x
+		xor j, j	; r13 = j = 0
 		.ciclo_columnas:
 			mov rdi, r15
 			mov rsi, i
@@ -68,13 +70,38 @@ aplicarSepia:
 
 	xor r12, r12
 	xor r13, r13
-	.ciclo:
 
-		; no hace nada pero aca habria que hacer la cuenta loca
+	pxor xmm0, xmm0
+	pxor xmm1, xmm1				; pongo todos estos registros en 0
+	pxor xmm7, xmm7
 
-		inc r13
-		cmp r13, 4
-	jne .ciclo
+	movd xmm0, [rdi]			; pongo en xmm0 los 32b del pixel (RGBA)
+
+	punpcklbw xmm0, xmm7		; separo los numeros asi si alguno se pasa de 255 no me pisa el siguiente
+
+								; aca arranco la sumatoria de R + G + B. Lo que me importa es el 1er numero en xmm0, ahi va a estar el resultado
+	movups xmm1, xmm0			; copio en xmm0 en xmm1 R | G | B | A
+
+	psrldq xmm1, 2				; shifteo xmm1, G | B | A
+
+	addps xmm0, xmm1			; xmm0 R + G | G | B | A
+	psrldq xmm1, 2				; shifteo xmm1, B | A
+	addps xmm0, xmm1			; xmm0 R + G + B | G | B | A
+
+	movups xmm1, xmm0			; copio el resultado en xmm1
+	mov r12, 0xFFFF
+	movq xmm7, r12
+	pand xmm1, xmm7				; y pongo todo el resto del registro en 0
+
+	movups xmm0, xmm1
+	pslldq xmm1, 2
+	paddw xmm0, xmm1
+	pslldq xmm1, 2
+	paddw xmm0, xmm1			; lo que hice aca es hacer que xmm0 sea SUMA | SUMA | SUMA
+
+	movdqu xmm7, [val0503]		; esto es para que xmm7 sea 0.5 | 0.3 (lo saque de la ultima clase, creo q entran dos numeros FP por registro)
+
+								; hasta aca llego mi amor
 
 	pop r12
 	pop r13
@@ -107,7 +134,7 @@ multiplicar:
 	pop rbp
 ret
 
-;pixel* matriz(matriz*, int i, int j, int #filas)
+;pixel* matriz(matriz*, int i, int j, int #columnas)
 matriz:
 	push rbp
 	mov rbp, rsp
