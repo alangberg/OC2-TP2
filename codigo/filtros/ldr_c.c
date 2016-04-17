49533e66812389598ea8a837934b277557bbb7c3
julianlen <julianlen@gmail.com>
2016-04-17 20:40:40 -0300

    ldr

--- codigo/filtros/ldr_c.c
+++ codigo/filtros/ldr_c.c
@@ -6,6 +6,7 @@
 
 #define P 2
 
+
 void ldr_c    (
     unsigned char *src,
     unsigned char *dst,
@@ -13,7 +14,7 @@ void ldr_c    (
     int filas,
     int src_row_size,
     int dst_row_size,
-	int alpha)
+    int alpha)
 {
     unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
     unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
@@ -27,6 +28,28 @@ void ldr_c    (
             *p_d = *p_s;
         }
     }
+    int max  = 5*5*255*3*255;
+    for (int i = 2; i < filas-2; i++){
+        for (int j = 2; j < cols-2; j++){
+            
+            float sumaRGB = 0;
+            for(int k = 0; k < 4 ; k++){
+                for(int l = 0; l < 4; l++){
+                    int fila = i-2+k;
+                    int col = (j-2+l)*4;
+                   sumaRGB += src_matrix[fila][col+0] + src_matrix[fila][col+1] + src_matrix[fila][col+2];
+                 }
+            }
+
+
+
+            float ldrB = src_matrix[i][j*4 + 0] + alpha * sumaRGB * src_matrix[i][j*4 + 0] / max;
+            float ldrG = src_matrix[i][j*4 + 1] + alpha * sumaRGB * src_matrix[i][j*4 + 1] / max;
+            float ldrR = src_matrix[i][j*4 + 2] + alpha * sumaRGB * src_matrix[i][j*4 + 2] / max;
+
+            dst_matrix[i][j*4 + 0] = (unsigned char) MIN(MAX(ldrB , 0) , 255);
+            dst_matrix[i][j*4 + 1] = (unsigned char) MIN(MAX(ldrG , 0) , 255);
+            dst_matrix[i][j*4 + 2] = (unsigned char) MIN(MAX(ldrR , 0) , 255);
+        }
+    }
 }
-
-
