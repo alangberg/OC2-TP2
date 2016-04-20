1bf3b78c7da49f8540686396843e0c2e773cddbe
Andres Langberg <andreslangberg@gmail.com>
2016-04-20 11:46:42 -0300

    Psyduck

--- codigo/filtros/ldr_asm.asm
+++ codigo/filtros/ldr_asm.asm
@@ -82,26 +82,26 @@ ret
 ;aplicarFiltroldr(src_rgba_t*, src_row_size)
 aplicarFiltroldr:
 
-	lea rdx, [rdi - 8]
-	mov r9, rsi
-	add r9, r9
+	lea rdx, [rdi - 8]					; rdx <- I(i,j-2)
+	mov r9, rsi 								
+	add r9, r9                  ; r9 <- src_row_size*2 
 
-	sub rdx, r9
+	sub rdx, r9 								; rdx <- I(i-2,j-2)
 	
 	xor r8, r8
 	pxor xmm14, xmm14
 
 	.ciclo:
-		movdqu xmm0, [rdx]			; pongo en xmm0 los 128b de los 4 pixeles - xmm0 = p[i-2,j-2] | p[i-2,j-1] | p[i-2,j] | p[i-2,j+1] 
+		movdqu xmm0, [rdx]				; pongo en xmm0 los 128b de los 4 pixeles - xmm0 = p[i-2,j-2] | p[i-2,j-1] | p[i-2,j] | p[i-2,j+1] 
 
 		pxor xmm7, xmm7
-		movdqu xmm15, xmm0					; xmm15 = p0 | p1 | p2 | p3
+		movdqu xmm15, xmm0				; xmm15 = p0 | p1 | p2 | p3
 
-		punpcklbw xmm0, xmm7				; xmm0 = 0 | a7 | . . . | 0 | a0
-		punpckhbw xmm15, xmm7				; xmm15 = 0 | a15 | . . . | 0 | a8
+		punpcklbw xmm0, xmm7			; xmm0 = 0 | a7 | . . . | 0 | a0
+		punpckhbw xmm15, xmm7			; xmm15 = 0 | a15 | . . . | 0 | a8
 
 		call sumarPixeles
-		paddd xmm14, xmm0					; me guardo el resultado en xmm14 (ojo! porq esta en dw)
+		paddd xmm14, xmm0					; xmm14 = R0+G0+B0 + R1+G1+B1 | 0 | 0 | 0 | 0 | 0 | 0 | 0
 
 		movups xmm0, xmm15
 		call sumarPixeles
@@ -115,13 +115,13 @@ aplicarFiltroldr:
 
 	xor r8, r8
 
-	lea rdx, [rdi + 8]
-	sub rdx, r9
+	lea rdx, [rdi + 8]					
+	sub rdx, r9									; rdx <- I(i-2,j+2)
 
 	.ciclo_2:
-		movd xmm0, [rdx]			; pongo en xmm0 los 4Bytes del pixel - xmm0 = p[i+2,j-2] | . | . | .
+		movd xmm0, [rdx]					; pongo en xmm0 los 4Bytes del pixel - xmm0 = p[i+2,j-2] | . | . | .
 		pxor xmm7, xmm7
-		punpcklbw xmm0, xmm7				; xmm0 = 0 | a7 | . . . | 0 | a0
+		punpcklbw xmm0, xmm7			; xmm0 = 0 | a7 | . . . | 0 | a0
 		call sumarPixel
 		paddd xmm14, xmm0					; sumo el resultado en xmm14 (ojo! porq esta en dw) xmm14 = sumaP0 + .. + sumaP3  | 0 | 0 | 0
 
@@ -187,7 +187,7 @@ ret
 sumarPixeles:
 
 	movups xmm1, xmm0					; xmm0 = R0 | G0 | B0 | A0 | R1 | G1 | B1 | A1
-								   		; xmm1 = R0 | G0 | B0 | A0 | R1 | G1 | B1 | A1
+								   					; xmm1 = R0 | G0 | B0 | A0 | R1 | G1 | B1 | A1
 							    
 	psrldq xmm1, 2						; shifteo xmm1 =  G0 | B0 | A0 | R1 | G1 | B1 | A1 | .
 
@@ -208,23 +208,28 @@ sumarPixeles:
 	psrldq xmm1, 8						; shifteo xmm1 = R1+G1+B1 | 0 | 0 | 0 | 0 | 0 | 0 | 0
 
 	pxor xmm7, xmm7
-	punpcklwd xmm0, xmm7				; xmm0 =
-	punpcklwd xmm1, xmm7				; xmm0 =
+	punpcklwd xmm0, xmm7			; xmm0 = R0+G0+B0 | 0 | R1+G1+B1 | 0  
+	punpcklwd xmm1, xmm7			; xmm1 = R1+G1+B1 | 0 | 0 | 0 
 
-	paddd xmm0, xmm1					; xmm0 = R0+G0+B0 + R1+G1+B1 | . | . | . | R1+G1+B1 | . | . | .
+	paddd xmm0, xmm1					; xmm0 = R0+G0+B0 + R1+G1+B1 | 0 | R1+G1+B1 | 0 
+	
+	pxor xmm7, xmm7
+	mov r12, 0xFFFF 					; mascara para setear todo en 0 menos las sumas. 
+	movq xmm7, r12						; xmm7 = 1 0 0 0 0 0 0 0
+	pand xmm0, xmm7						; xmm0 = R0+G0+B0 + R1+G1+B1 | 0 | 0 | 0 | 0 | 0 | 0 | 0
 ret
 
 sumarPixel:
-		movups xmm1, xmm0					; xmm1 = R0 | G0 | B0 | A0 | . | . | . | .
-		psrldq xmm1, 2			;shifteo xmm1 =  G0 | B0 | A0 | . | . | . | . | .
-		paddw xmm0, xmm1					; xmm0 = R0+G0 | . | . | . | . | . | . | .
-		psrldq xmm1, 2			; shifteo xmm1 = B0 | A0 | . | . | . | . | . | .
-		paddw xmm0, xmm1					; xmm0 = R0+G0+B0 | . | . | . | . | . | . | .
+		movups xmm1, xmm0				; xmm1 = R0 | G0 | B0 | A0 | . | . | . | .
+		psrldq xmm1, 2					; shifteo xmm1 =  G0 | B0 | A0 | . | . | . | . | .
+		paddw xmm0, xmm1				; xmm0 = R0+G0 | . | . | . | . | . | . | .
+		psrldq xmm1, 2					; shifteo xmm1 = B0 | A0 | . | . | . | . | . | .
+		paddw xmm0, xmm1				; xmm0 = R0+G0+B0 | . | . | . | . | . | . | .
 		
 		pxor xmm7, xmm7
-		mov r12, 0xFFFF 					; mascara para setear todo en 0 menos las sumas. 
-		movq xmm7, r12						; xmm7 = 1 0 0 0 0 0 0 0
-		pand xmm0, xmm7						; xmm0 = R0+G0+B0 | 0 | 0 | 0 | 0 | 0 | 0 | 0
+		mov r12, 0xFFFF 				; mascara para setear todo en 0 menos las sumas. 
+		movq xmm7, r12					; xmm7 = 1 0 0 0 0 0 0 0
+		pand xmm0, xmm7					; xmm0 = R0+G0+B0 | 0 | 0 | 0 | 0 | 0 | 0 | 0
 ret
 
 
